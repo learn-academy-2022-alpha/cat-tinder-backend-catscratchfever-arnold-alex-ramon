120091882b80abe9df3a805a83e9d0d5a9163fe0
Arnold Laoang <arnoldlaoangjr@gmail.com>
2022-04-11 14:06:41 -0700

    validations tasks

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -94,5 +94,31 @@ RSpec.describe "Cats", type: :request do
       expect(response).to have_http_status(422)
       expect(cat['age']).to include "can't be blank"
     end
+    it 'cannot create a cat without an age' do
+      cat_params = {
+        cat: {
+          name: 'Alisha',
+          enjoys: 'Being a cat',
+          image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+          }
+      }
+      post '/cats', params: cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['age']).to include "can't be blank"
+    end
+    it 'cannot create a cat without an enjoys' do
+      cat_params = {
+        cat: {
+          name: 'Alisha',
+          age: 24,
+          image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+          }
+      }
+      post '/cats', params: cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['enjoys']).to include "can't be blank"
+    end
   end
 end
