29aea3ffb8295e44075949a7955d9f640116a171
Alex Ranga <alexisranga@gmail.com>
2022-04-10 22:13:08 -0700

    completed API endpopints

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -1,7 +1,84 @@
+
+   
 require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Cat.create(
+        name: 'Felina',
+        age: 12,
+        enjoys: 'Being a cat',
+        image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+      )
+
+      get '/cats'
+
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
   end
+
+  describe "POST /create" do
+    it "creates a cat" do
+
+      cat_params = {
+        cat: {
+          name: 'Felina',
+          age: 12,
+          enjoys: 'Being a cat',
+          image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+        }
+      }
+
+      
+      post '/cats', params: cat_params
+
+      
+      expect(response).to have_http_status(200)
+
+      
+      cat = Cat.first
+
+     
+      expect(cat.name).to eq 'Felina'
+      expect(cat.age).to eq 12
+      expect(cat.enjoys).to eq 'Being a cat'
+      expect(cat.image).to eq 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+    end
+  end
+  describe 'PATCH /update' do
+    it 'updates a cat' do
+      Cat.create name: 'Felina', age: 12, enjoys: 'Being a cat', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+
+      cat_felina = Cat.first
+
+      cat_params = {
+        cat: {
+          name: 'Alisha',
+          age: 24,
+          enjoys: 'Being a cat',
+          image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+          }
+        }
+        patch "/cats/#{cat_felina.id}", params: cat_params
+        cat_mystery = Cat.find(cat_felina.id)
+
+        expect(response).to have_http_status(200)
+        expect(cat_mystery.name).to eq 'Alisha'
+    end
+  end
+  describe 'DELETE /destriy' do
+    it 'deletes a cat from the DB' do
+      Cat.create name: 'Felina', age: 12, enjoys: 'Being a cat', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-cats-are-best-pets-1559241235.jpg?crop=0.586xw:0.880xh;0.0684xw,0.0611xh&resize=640:*'
+      cat_felina = Cat.first
+      delete "/cats/#{cat_felina.id}"
+      expect(response).to have_http_status(200)
+
+      cats = Cat.all
+      expect(cats).to be_empty
+    end
+  end
+
 end
